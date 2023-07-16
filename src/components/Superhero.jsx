b9028c6e54ebbd64be3e27d450be1fd66050ebd3
Тайсумова Оксана <119331948+Oksana2211@users.noreply.github.com>
2023-07-16 23:24:09 +0300

    add faile

---
+++ src/components/Superhero.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../style/Superhero.css';
+
+function Superhero(props) {
+    return(
+        <section className ="card">
+            <div className="name">{props.name}</div>
+            <div className="text">Вселенная: {props.universe}</div>
+            <div className="text">Альтерэго: {props.alterego}</div>
+            <div className="text">Вид деятельности: {props.occupation}</div>
+            <div className="text">Друзья: {props.friends}</div>
+            <div className="text">Суперспособности: {props.superpowers}</div>
+            <img className="img" src={props.url}></img>
+            <div className="text">{props.info}</div>
+        </section>
+    )
+}
+
+export default Superhero;
